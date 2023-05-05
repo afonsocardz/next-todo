1e01bfdca884b14a6c2d6b7ec37975721632281d
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2023-05-05 13:27:46 -0300

    feat: add todo list component

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,27 @@
+import { TodoReturnData } from "@/interfaces";
+import TodoItem from "./TodoItem";
+import { CSSProperties } from "react";
+
+interface TodoListProps {
+  todos: TodoReturnData[];
+}
+
+export default function TodoList({ todos }: TodoListProps) {
+  return (
+    <div style={listStyle}>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </div>
+  );
+}
+
+const listStyle: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "12px",
+  padding: "12px",
+  backgroundColor: "white",
+  overflow: "scroll",
+  height: "100%",
+};
