fb302d4daa245f527d5ce0908d55914346c070cf
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2023-05-05 13:27:21 -0300

    feat: add global layout handler

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Header, { headerStyles } from "@/layouts/Header";
+import "@/styles/globals.css";
+import { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { CSSProperties, ReactElement, ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+  return (
+    <>
+      <Header />
+      <div style={defaultStyle}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
+
+const defaultStyle: CSSProperties = {
+  marginTop: headerStyles.height,
+  padding: "12px",
+  backgroundColor: "lightgray",
+  height: "100vh",
+};
